0b32dc1ba1d2a6216e692aef06fe263e001ffca4
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-09 09:02:06 +0530

    Visual Changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,16 +8,16 @@ const Hero = () => {
         <Image src={HeroImage} property="true" layout="responsive" alt="Hero" />
         <div className="absolute top-[22%] right-0 -z-50 h-[12rem] w-[80%] rounded-l-full bg-soft-blue" />
       </div>
-      <article className="space-y-5 text-center pt-6">
+      <article className="space-y-5 pt-6 text-center">
         <h1 className="text-3xl font-medium text-dark-blue">
           A Simple Bookmark Manager
         </h1>
-        <p className="text-grayish-blue">
+        <p className="text-grayish-blue line-clamp-4">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </p>
       </article>
-      <div className="space-x-2 mt-8">
+      <div className="mt-8 space-x-2">
         <button className="btn-primary">Get it on Chrome</button>
         <button className="btn-secondary">Get it on Firefox</button>
       </div>
