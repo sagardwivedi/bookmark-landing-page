6737e895747a267a228536d7f131c56c159a12e4
sagardwivedi <Sagar.dw20@gmail.com>
2022-06-09 08:55:39 +0530

    Changes: UI

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div>
-      <div>
+      <div className="my-8">
         <Image src={HeroImage} property="true" layout="responsive" alt="Hero" />
+        <div className="absolute top-[22%] right-0 -z-50 h-[12rem] w-[80%] rounded-l-full bg-soft-blue" />
       </div>
-      <article className="text-center">
-        <h1 className="text-4xl font-medium text-dark-blue">
+      <article className="space-y-5 text-center pt-6">
+        <h1 className="text-3xl font-medium text-dark-blue">
           A Simple Bookmark Manager
         </h1>
         <p className="text-grayish-blue">
@@ -16,7 +17,7 @@ const Hero = () => {
           a new browser tab and see your sites load instantly. Try it for free.
         </p>
       </article>
-      <div>
+      <div className="space-x-2 mt-8">
         <button className="btn-primary">Get it on Chrome</button>
         <button className="btn-secondary">Get it on Firefox</button>
       </div>
